86eeb5480a641455277ada8b08a6adc3647b299b
qwattash <mzz.lrd@gmail.com>
2014-03-11 02:11:54 +0000

    Added anrem target scoped path

--- mk/functions.mk
+++ mk/functions.mk
@@ -2,7 +2,6 @@
 # Library functions for inclusion and utilities
 #
 
-
 # include given modules, this function MUST be used to include
 # the project modules
 anrem-include-modules = $(foreach ANREM_CURRENT_MODULE,$(1),$(eval -include $(ANREM_CURRENT_MODULE)/module.mk))
@@ -16,15 +15,26 @@ anrem-current-path = $(ANREM_CURRENT_MODULE)
 # add given target to the global targets list
 # that is executed when make all is run
 #
-anrem-build = $(eval ANREM_BUILD_TARGETS += $(addprefix $(ANREM_CURRENT_MODULE)/,$(strip $(1))))
+anrem-build = $(eval ANREM_BUILD_TARGETS += $(call anrem-join, $(1)))
 
 #
 # add given target to the clean list
 # that is executed whenever make clean is issued
 #
-anrem-build-clean = $(eval ANREM_BUILD_CLEAN += $(addprefix $(ANREM_CURRENT_MODULE)/,$(strip $(1))))
+anrem-build-clean = $(eval ANREM_BUILD_CLEAN += $(call anrem-join, $(1)))
+
+#
+# define the local path for a given target
+#
+anrem-target-defpath = $(eval $(1): path:=$(ANREM_CURRENT_MODULE))
 
 #
 # define a target relative to current path
+# @param $1 target absolute name
 #
-anrem-target = $(addprefix $(ANREM_CURRENT_MODULE)/,$(1))
+anrem-target = $(strip $(1))$(call anrem-target-defpath, $(strip $(1)))
+
+#
+# join relative path with absolute path, safe usage outside make rules
+# 
+anrem-join = $(addprefix $(ANREM_CURRENT_MODULE)/,$(strip $(1)))
