47083c753ee890817ea0ad45928a68976f5521cf
xEnVrE <nicolapiga@gmail.com>
2014-08-26 23:50:12 +0200

    Moved some vars from project.mk to bootloader/module.mk.

--- bootloader/module.mk
+++ bootloader/module.mk
@@ -2,18 +2,36 @@
 # Top level makefile for the bootloader module
 #
 
+#paths
 CURRENT := $(call anrem-current-path)
+BOOT_DIR := $(CURRENT)/boot
+HDD_VDI := $(BOOT_DIR)/hdd.vdi
+HDD_IMG := $(BOOT_DIR)/hdd.img
 
-boot_BUILD_DIR := $(CURRENT)/boot
-
-#module specific toolchain and toolchain options
-
-#define module params
-HDD_VDI := $(boot_BUILD_DIR)/hdd.vdi
-HDD_IMG := $(boot_BUILD_DIR)/hdd.img
-#external params
+#external modules
 STAGE1_IN := $(CURRENT)/stage1/mbr.sect
 STAGE2_IN := $(CURRENT)/stage2/stage2.out
+
+#VirtualBox vars
+VBOX := VirtualBox
+VBOXFLAGS := --debug --startvm Kernel
+
+#VBoxManage vars
+VBOXMANAGE := VBoxManage
+VBOXMANAGEFLAGS := convertfromraw --uuid 84e10c7b-5fa0-4c91-9ed1-722b372570ee -format VDI
+
+#qemu-img
+QEMUIMG := qemu-img
+QEMUIMGFLAGS := convert -f vdi -O raw
+
+#disk-mgmt-tool vars
+DISKMGMT := python bootloader/utils/disk-mgmt-tool/diskmgmt.py
+HDDSIZE := 128 # in MiB
+#first partition: Size 64MiB; Sectors 2048-133120; Type FAT32; Index 1.
+FIRSTP_START := 2048
+FIRSTP_END := 133120
+FIRSTP_FS := fat32
+FIRSTP_INDEX := 1
 
 $(call anrem-build, $(HDD_VDI)) : $(HDD_IMG)
 	rm -f $@
@@ -21,8 +39,7 @@ $(call anrem-build, $(HDD_VDI)) : $(HDD_IMG)
 
 $(call anrem-target, $(HDD_IMG)): $(STAGE1_IN) $(HDD_VDI).orig
 	rm -f $@
-	@#$(VBOXMANAGE) clonehd $(HDD_VDI) $@ --format RAW
-	qemu-img convert -f vdi $(HDD_VDI) -O raw $@
+	$(QEMUIMG) $(QEMUIMGFLAGS) $(HDD_VDI) $@
 	$(DISKMGMT) mbr $@ $(STAGE1_IN)
 
 $(call anrem-target, $(HDD_VDI).orig) :
--- project.mk
+++ project.mk
@@ -1,3 +1,7 @@
+#
+# kernel Project makefile
+#
+
 #architecture
 ARCH := i386
 
@@ -11,27 +15,4 @@ ASMFLAGS :=
 LDFLAGS := -m elf_i386
 LDASMFLAGS := 
 
-#
-# configuration variables for VirtualBox and VBoxManage
-#
-VBOXMINSIZE := 1048576
-VBOX := VirtualBox
-VBOXFLAGS := --debug --startvm Kernel
-VBOXMANAGE := VBoxManage
-VBOXMANAGEFLAGS := convertfromraw --uuid 84e10c7b-5fa0-4c91-9ed1-722b372570ee -format VDI
-
-#
-# configuration variables for disk-mgmt-tool
-#
-DISKMGMT := python bootloader/utils/disk-mgmt-tool/diskmgmt.py
-HDDSIZE := 128 # in MiB
-##
-## 1st partition
-## 64 MiB (sectors 2048 - 133120)
-## fat32
-##
-FIRSTP_START := 2048
-FIRSTP_END := 133120
-FIRSTP_FS := fat32
-FIRSTP_INDEX := 1
 
