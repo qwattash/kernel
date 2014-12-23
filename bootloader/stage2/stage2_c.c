c484d37d7408955dca4e9e4b0240ef514d6cd4fa
xEnVrE <nicolapiga@gmail.com>
2014-12-23 01:49:49 +0100

    i) Bug fixes in make_gdt_entry

--- bootloader/stage2/stage2_c.c
+++ bootloader/stage2/stage2_c.c
@@ -2,12 +2,18 @@
  * this loads the stage 3 image from the boot partition
 */
 
+#define VIDEO_MEM 0xB8000
+
 /** Entry point, called by stage 2 assembly section as soon as it is executed by stage 1
 */
 void cmain()
 {
-  while (1)
+    char* video_ptr = VIDEO_MEM;
+    video_ptr += 2*80;
+    *(video_ptr) = 'K';
+
+    while (1)
     {
-      continue;
+	continue;
     }
 }
